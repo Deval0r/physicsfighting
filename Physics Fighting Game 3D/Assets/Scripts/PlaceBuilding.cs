ff555dcb8729ce72caeed44e7f89792cd4d1e32b
Andrew Platosh <aplatosh6982@student.pps.net>
2025-03-12 18:35:19 -0700

    prefab buildings snap to grid

--- Physics Fighting Game 3D/Assets/Scripts/PlaceBuilding.cs
+++ Physics Fighting Game 3D/Assets/Scripts/PlaceBuilding.cs
@@ -5,10 +5,13 @@ public class PlaceBuilding : MonoBehaviour
     private Vector3 mousePosition;
     private Vector3 buildingPosition;
     public GameObject buildingPrefab;
+    private bool isSelectedBuilding;
+
+    [SerializeField] private int scaleFactor;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -17,14 +20,14 @@ public class PlaceBuilding : MonoBehaviour
         PlaceBuildings();
     }
 
-    private void PlaceBuildings()
+    public void PlaceBuildings()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(mouseRay, out RaycastHit hit))
             {
-                buildingPosition = new Vector3(Mathf.Round(hit.point.x), Mathf.Round(hit.point.y), Mathf.Round(hit.point.z));
+                buildingPosition = new Vector3(Mathf.Round(hit.point.x/scaleFactor) * scaleFactor, Mathf.Round(hit.point.y/scaleFactor) * scaleFactor, Mathf.Round(hit.point.z/scaleFactor)*scaleFactor );
                 Instantiate(buildingPrefab, buildingPosition, Quaternion.identity);
             }
         }
