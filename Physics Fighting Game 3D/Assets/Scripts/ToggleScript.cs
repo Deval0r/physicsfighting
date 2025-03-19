0e0a0ef8aaa52012116662f7c810ae009a1d9e75
Biscuit002 <aplatosh6982@student.pps.net>
2025-03-19 10:32:22 -0700

    merge

--- Physics Fighting Game 3D/Assets/Scripts/ToggleScript.cs
+++ Physics Fighting Game 3D/Assets/Scripts/ToggleScript.cs
@@ -2,26 +2,33 @@ using UnityEngine;
 
 public class ToggleScript : MonoBehaviour
 {
-    [SerializeField] private GameObject TPCamera;
-    [SerializeField] private GameObject FPCamera;
+    [SerializeField] private GameObject TPObject;
+    [SerializeField] private GameObject FPObject;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        TPObject.SetActive(true);
+        FPObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P) && TPCamera.activeSelf)
+        if (Input.GetKeyDown(KeyCode.P) && TPObject.activeSelf)
         {
-            TPCamera.SetActive(false);
-            FPCamera.SetActive(true);
+            TPObject.SetActive(false);
+            FPObject.SetActive(true);
         }
-        else if (Input.GetKeyDown(KeyCode.P) && FPCamera.activeSelf)
+        else if (Input.GetKeyDown(KeyCode.P) && FPObject.activeSelf)
         {
-            TPCamera.SetActive(true);
-            FPCamera.SetActive(false);
+            TPObject.SetActive(true);
+            FPObject.SetActive(false);
+        }
+
+        if (FPObject.activeSelf)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
     }
 }
