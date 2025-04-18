62ed7ced1e66af20d5263b01463b6ec6ee2c2c9c
Andrew Platosh <aplatosh6982@student.pps.net>
2025-04-17 22:37:32 -0700

    a

--- Physics Fighting Game 3D/Assets/Scripts/Food.cs
+++ Physics Fighting Game 3D/Assets/Scripts/Food.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
+    public PlayerHealth playerHealth;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerHealth = FindObjectOfType<PlayerHealth>();
     }
 
     // Update is called once per frame
@@ -18,6 +20,11 @@ public class Food : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Destroy(gameObject);
+            playerHealth.currentHealth += 10;
+            if (playerHealth.currentHealth > playerHealth.maxHealth)
+            {
+                playerHealth.currentHealth = playerHealth.maxHealth;
+            }
         }
     }
 }
